5cb7159893e520f9a09ef352c1060e89b1c3e236
sagethecoder <162547892+sagethecoder@users.noreply.github.com>
2024-07-05 12:01:48 +0530

    Update FormPage.jsx

--- src/page/FormPage.jsx
+++ src/page/FormPage.jsx
@@ -85,7 +85,6 @@ const Form = () => {
   return (
     <div
       className="min-h-screen bg-cover bg-center bg-custom-image py-12 px-4 sm:px-6 lg:px-8"
-{/*       style={{ backgroundImage: "url('../../src/assets/willoberry.jpg')" }} */}
     >
       <div className="max-w-md mx-auto bg-white bg-opacity-90 rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
         <form onSubmit={handleSubmit} className="p-8">
