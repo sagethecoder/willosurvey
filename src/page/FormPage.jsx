538c91669dc7065b51db1aae19dba88bee34870b
sagethecoder <162547892+sagethecoder@users.noreply.github.com>
2024-07-05 15:40:13 +0530

    Update Some basic Info in FormPage.jsx

--- src/page/FormPage.jsx
+++ src/page/FormPage.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from "react";
 import questions from "../../utils/data";
 import { useNavigate } from "react-router-dom";
+import toast, { Toaster } from 'react-hot-toast';
+
 const Form = () => {
   const [responses, setResponses] = useState({});
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
   const [loggedUser, setLoggedUser] = useState("");
   const [isLoading, setLoading] = useState(null);
+  const [basicInfo, setBasicInfo] = useState({
+    name: "",
+    region: "",
+    territory: "",
+    farmer_name: "",
+    contact: "",
+    area_of_soybean: "",
+    patro_qty: ""
+  });
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -15,6 +26,25 @@ const Form = () => {
     }
   }, []);
 
+  const handleBasicInfoChange = (e) => {
+    const { name, value } = e.target;
+    if(name =="contact"){
+      const regex = /^[0-9\b]{0,10}$/;
+      if(value ==="" || regex.test(value)){
+        setBasicInfo((prevInfo) => ({
+          ...prevInfo,
+          [name]: value
+        }));
+      }
+    }else {
+      setBasicInfo((prevInfo) => ({
+        ...prevInfo,
+        [name]: value
+      }));
+    }
+    
+  };
+
   const handleResponse = (questionId, value) => {
     setResponses((prev) => ({
       ...prev,
@@ -23,7 +53,21 @@ const Form = () => {
   };
 
   const handleNext = () => {
-    if (currentQuestionIndex < questions.length - 1) {
+    if (currentQuestionIndex === 0) {
+      if (
+        basicInfo.name &&
+        basicInfo.territory &&
+        basicInfo.region &&
+        basicInfo.farmer_name &&
+        basicInfo.contact &&
+        basicInfo.area_of_soybean &&
+        basicInfo.patro_qty
+      ) {
+        setCurrentQuestionIndex(1);
+      } else {
+        toast.error("Please fill all basic information fields");
+      }
+    } else if (currentQuestionIndex < questions.length) {
       setCurrentQuestionIndex((prev) => prev + 1);
     }
   };
@@ -34,14 +78,19 @@ const Form = () => {
     }
   };
 
-  const currentQuestion = questions[currentQuestionIndex];
+  const currentQuestion = questions[currentQuestionIndex - 1];
 
   const handleSubmit = async (e) => {
     setLoading(true);
-
     e.preventDefault();
     const dataToSend = {
-      name: loggedUser.name,
+      name: basicInfo.name,
+      region: basicInfo.region,
+      territory: basicInfo.territory,
+      farmerName: basicInfo.farmer_name,
+      contact: basicInfo.contact,
+      area_of_soybean: basicInfo.area_of_soybean,
+      patro_qty: basicInfo.patro_qty,
       email: loggedUser.email,
       question1: responses[1] || "",
       question2: responses[2] || "",
@@ -55,7 +104,7 @@ const Form = () => {
       question10: responses[10] || ""
     };
     console.log("datasend", dataToSend);
-    // return
+    // return;
     try {
       const response = await fetch("https://sheetdb.io/api/v1/ftdo91gp1xfc2", {
         method: "POST",
@@ -72,10 +121,10 @@ const Form = () => {
       if (result) {
         setLoading(false);
         setTimeout(() => {
-          navigate('/thank');
+          navigate("/thank");
         }, 1500);
       }
-     
+
       console.log("Data appended:", result);
     } catch (error) {
       console.error("Error appending data:", error);
@@ -83,38 +132,113 @@ const Form = () => {
   };
 
   return (
-    <div
-      className="min-h-screen bg-cover bg-center bg-custom-image py-12 px-4 sm:px-6 lg:px-8">
+    <div className="min-h-screen bg-cover bg-center bg-custom-image py-12 px-4 sm:px-6 lg:px-8 font-serif">
+      <Toaster position="bottom-center" reverseOrder={false} />
       <div className="max-w-md mx-auto bg-white bg-opacity-90 rounded-xl shadow-lg overflow-hidden md:max-w-2xl">
         <form onSubmit={handleSubmit} className="p-8">
-          <div className="mb-6 ">
-            <div className="rounded ">
-              <img
-                src={currentQuestion.image}
-                alt={`Question ${currentQuestion.id}`}
-                className="w-full h-[14rem] object-contain aspect-square rounded-lg mb-6"
-              />
+          {currentQuestionIndex === 0 && (
+            <div className="mb-6">
+              <h2 className="text-2xl font-bold text-green-800 mb-4">Basic Information</h2>
+              <div className="space-y-4">
+                <input
+                  type="text"
+                  name="name"
+                  value={basicInfo.name}
+                  onChange={handleBasicInfoChange}
+                  placeholder="Name of Willowood Employee"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+                <input
+                  type="text"
+                  name="region"
+                  value={basicInfo.region}
+                  onChange={handleBasicInfoChange}
+                  placeholder="Region"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+                <input
+                  type="text"
+                  name="territory"
+                  value={basicInfo.territory}
+                  onChange={handleBasicInfoChange}
+                  placeholder="Territory"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+                <input
+                  type="text"
+                  name="farmer_name"
+                  value={basicInfo.farmer_name}
+                  onChange={handleBasicInfoChange}
+                  placeholder="Farmer Name"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+                <input
+                  type="tel"
+                  name="contact"
+                  maxLength={10}
+                  value={basicInfo.contact}
+                  onChange={handleBasicInfoChange}
+                  placeholder="Contact"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+                <input
+                  type="text"
+                  name="area_of_soybean"
+                  value={basicInfo.area_of_soybean}
+                  onChange={handleBasicInfoChange}
+                  placeholder="Area of Soybean"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+                <input
+                  type="number"
+                  name="patro_qty"
+                  value={basicInfo.patro_qty}
+                  onChange={handleBasicInfoChange}
+                  placeholder="Patromax purchased qty"
+                  className="w-full p-2 border border-gray-300 rounded"
+                  required
+                />
+              </div>
             </div>
-            <h2 className="text-2xl font-bold text-green-800 mb-4">{currentQuestion.text}</h2>
-            <div className="space-y-4">
-              {["yes", "no"].map((option) => (
-                <label
-                  key={option}
-                  className="flex items-center space-x-3 cursor-pointer hover:bg-green-100 p-2 rounded-lg transition-colors"
-                >
-                  <input
-                    type="radio"
-                    name={`question_${currentQuestion.id}`}
-                    value={option}
-                    checked={responses[currentQuestion.id] === option}
-                    onChange={() => handleResponse(currentQuestion.id, option)}
-                    className="form-radio h-5 w-5 text-green-600"
-                  />
-                  <span className="text-gray-700 font-medium capitalize">{option}</span>
-                </label>
-              ))}
+          )}
+
+          {currentQuestionIndex > 0 && (
+            <div className="mb-6">
+              <div className="rounded">
+                <img
+                  src={currentQuestion.image}
+                  alt={`Question ${currentQuestion.id}`}
+                  className="w-full h-[14rem] object-contain aspect-square rounded-lg mb-6"
+                />
+              </div>
+              <h2 className="text-2xl font-bold text-green-800 mb-4">{currentQuestion.text}</h2>
+              <div className="space-y-4">
+                {["हाँ ", "नहीं ", "खेत में नहीं है"].map((option) => (
+                  <label
+                    key={option}
+                    className="flex items-center space-x-3 cursor-pointer hover:bg-green-100 p-2 rounded-lg transition-colors"
+                  >
+                    <input
+                      type="radio"
+                      name={`question_${currentQuestion.id}`}
+                      value={option}
+                      checked={responses[currentQuestion.id] === option}
+                      onChange={() => handleResponse(currentQuestion.id, option)}
+                      className="form-radio h-5 w-5 text-green-600"
+                    />
+                    <span className="text-gray-700 font-medium capitalize">{option}</span>
+                  </label>
+                ))}
+              </div>
             </div>
-          </div>
+          )}
+
           <div className="flex justify-between">
             {currentQuestionIndex > 0 && (
               <button
@@ -125,13 +249,13 @@ const Form = () => {
                 Previous
               </button>
             )}
-            {currentQuestionIndex < questions.length - 1 ? (
+            {currentQuestionIndex < questions.length ? (
               <button
                 type="button"
                 onClick={handleNext}
-                disabled={!responses[currentQuestion.id]}
+                disabled={currentQuestionIndex > 0 && !responses[currentQuestion.id]}
                 className={`${
-                  responses[currentQuestion.id]
+                  currentQuestionIndex === 0 || responses[currentQuestion.id]
                     ? "bg-green-600 hover:bg-green-700"
                     : "bg-green-400 cursor-not-allowed"
                 } text-white font-bold py-2 px-6 rounded-full transition-colors`}
@@ -157,4 +281,5 @@ const Form = () => {
     </div>
   );
 };
+
 export default Form;
