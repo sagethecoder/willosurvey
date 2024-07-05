00f1ac3f292b719b788890b6891928437cd28620
sagethecoder <162547892+sagethecoder@users.noreply.github.com>
2024-07-05 11:54:34 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+ backgroundImage: {
+        'custom-image': "url('/src/assets/willoberry.jpg')",
+      },
+},
   },
   plugins: [],
 }
